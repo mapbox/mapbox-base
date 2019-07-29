1bebecef3babb7aec40aa57a3bffd9f765c072ce
Bruno de Oliveira Abinader <bruno@mapbox.com>
2019-07-29 11:56:40 +0300

    [build] optional @ f6249e7

---
+++ libs/optional.cmake
@@ -0,0 +1,14 @@
+if(TARGET mapbox-base-optional)
+    return()
+endif()
+
+execute_process(
+    COMMAND git submodule update --init optional
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+)
+
+add_library(mapbox-base-optional INTERFACE)
+
+target_include_directories(mapbox-base-optional SYSTEM INTERFACE
+    ${CMAKE_CURRENT_SOURCE_DIR}/optional
+)
--- test/include.cpp
+++ test/include.cpp
@@ -1,8 +1,11 @@
 #include <mapbox/geometry.hpp>
 #include <mapbox/variant.hpp>
 
+#include <optional.hpp>
+
 int main() {
     mapbox::geometry::geometry<int> geometry;
+    std::experimental::optional<int> optional;
     mapbox::util::variant<int> variant;
 
     return 0;

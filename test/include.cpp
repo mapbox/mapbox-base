bc2bd06d05886a198ce8bca652d417ad34d640af
Bruno de Oliveira Abinader <bruno@mapbox.com>
2019-08-15 17:06:07 +0300

    [base] args @ 401663c1d57006566db6d8e5b37a21018fbd0bc3

--- test/include.cpp
+++ test/include.cpp
@@ -1,13 +1,16 @@
 #include <mapbox/geometry.hpp>
 #include <mapbox/variant.hpp>
 
+#include <args.hxx>
 #include <optional.hpp>
 
 int main() {
+    args::ArgumentParser argumentParser("Mapbox Base Test");
     mapbox::geometry::geometry<int> geometry;
     std::experimental::optional<int> optional;
     mapbox::util::variant<int> variant;
 
+    (void)argumentParser;
     (void)geometry;
     (void)optional;
     (void)variant;

965f1905c19ed751f8328afd7569eb400ed7c86e
Bruno de Oliveira Abinader <bruno@mapbox.com>
2019-08-15 17:16:45 +0300

    [base] expected-lite @ 6fad61b14b9ec94efca57505bffea5f5de71cb53

--- test/include.cpp
+++ test/include.cpp
@@ -3,14 +3,17 @@
 
 #include <args.hxx>
 #include <optional.hpp>
+#include <nonstd/expected.hpp>
 
 int main() {
     args::ArgumentParser argumentParser("Mapbox Base Test");
+    nonstd::expected<void,int> expected;
     mapbox::geometry::geometry<int> geometry;
     std::experimental::optional<int> optional;
     mapbox::util::variant<int> variant;
 
     (void)argumentParser;
+    (void)expected;
     (void)geometry;
     (void)optional;
     (void)variant;

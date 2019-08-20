51400cfdfb43be58db20e4940a0117b69146d0e9
Bruno de Oliveira Abinader <bruno@mapbox.com>
2019-08-20 11:06:30 +0300

    [base] Added mapbox/io

---
+++ mapbox/io/include/mapbox/io.hpp
@@ -0,0 +1,73 @@
+#include <string>
+
+#include <ghc/filesystem.hpp>
+#include <nonstd/expected.hpp>
+
+#include <fstream>
+#include <sstream>
+#include <cerrno>
+#include <iostream>
+
+namespace mapbox {
+namespace util {
+
+using ErrorType = std::string;
+
+nonstd::expected<std::string, ErrorType> readFile(const ghc::filesystem::path& filename) {
+    nonstd::expected<std::string, ErrorType> result;
+
+    std::ifstream file(filename.string(), std::ios::binary);
+    if (!file.good()) {
+        return nonstd::make_unexpected(std::string("Failed to read file '") + filename.string() + std::string("'"));
+    }
+
+    std::stringstream data;
+    data << file.rdbuf();
+    result = data.str();
+    return result;
+}
+
+nonstd::expected<void, ErrorType> writeFile(const ghc::filesystem::path& filename, const std::string& data) {
+    nonstd::expected<void, ErrorType> result;
+
+    FILE *fd = fopen(filename.string().c_str(), "wb");
+    if (!fd) {
+        return nonstd::make_unexpected(std::string("Failed to write file '") + filename.string() + std::string("'"));
+    }
+
+    fwrite(data.data(), sizeof(std::string::value_type), data.size(), fd);
+    fclose(fd);
+    return result;
+}
+
+nonstd::expected<void, ErrorType> deleteFile(const ghc::filesystem::path& filename) {
+    nonstd::expected<void, ErrorType> result;
+
+    const int ret = std::remove(filename.string().c_str());
+    if (ret != 0) {
+        return nonstd::make_unexpected(std::string("Failed to delete file '") + filename.string() + std::string("'"));
+    }
+
+    return result;
+}
+
+
+nonstd::expected<void, ErrorType> copyFile(const ghc::filesystem::path& sourcePath, const ghc::filesystem::path& destinationPath) {
+    nonstd::expected<void, ErrorType> result;
+
+    std::ifstream source(sourcePath, std::ios::binary);
+    if (!source.good()) {
+        return nonstd::make_unexpected(std::string("Failed to open source file '") + sourcePath.string() + std::string("'"));
+    }
+
+    std::ofstream destination(destinationPath, std::ios::binary);
+    if (!destination.good()) {
+        return nonstd::make_unexpected(std::string("Failed to open destination file '") + destinationPath.string() + std::string("'"));
+    }
+
+    destination << source.rdbuf();
+    return result;
+}
+
+} // namespace util
+} // namespace mapbox
---
+++ test/io.cpp
@@ -0,0 +1,50 @@
+#include <mapbox/io.hpp>
+
+#include <cassert>
+
+int main() {
+    ghc::filesystem::path path(std::string(TEST_BINARY_PATH) + "foo.txt");
+    ghc::filesystem::path copyPath(std::string(TEST_BINARY_PATH) + "bar.txt");
+    ghc::filesystem::path invalidPath("invalid");
+    ghc::filesystem::path unauthorizedPath("/root/unauthorized");
+
+    std::string bar("bar");
+
+    nonstd::expected<void, std::string> voidExpected = mapbox::util::writeFile(path, bar);
+    assert(voidExpected);
+
+    voidExpected = mapbox::util::writeFile(unauthorizedPath, bar);
+    assert(!voidExpected);
+    assert(voidExpected.error() == std::string("Failed to write file '/root/unauthorized'"));
+
+    nonstd::expected<std::string, std::string> stringExpected = mapbox::util::readFile(path);
+    assert(stringExpected);
+    assert(*stringExpected == bar);
+
+    stringExpected = mapbox::util::readFile(invalidPath);
+    assert(!stringExpected);
+    assert(stringExpected.error() == std::string("Failed to read file 'invalid'"));
+
+    voidExpected = mapbox::util::copyFile(path, copyPath);
+    assert(voidExpected);
+
+    voidExpected = mapbox::util::copyFile(path, unauthorizedPath);
+    assert(!voidExpected);
+    assert(voidExpected.error() == std::string("Failed to open destination file '/root/unauthorized'"));
+
+    voidExpected = mapbox::util::copyFile(invalidPath, path);
+    assert(!voidExpected);
+    assert(voidExpected.error() == std::string("Failed to open source file 'invalid'"));
+
+    voidExpected = mapbox::util::deleteFile(path);
+    assert(voidExpected);
+
+    voidExpected = mapbox::util::deleteFile(copyPath);
+    assert(voidExpected);
+
+    voidExpected = mapbox::util::deleteFile(invalidPath);
+    assert(!voidExpected);
+    assert(voidExpected.error() == std::string("Failed to delete file 'invalid'"));
+
+    return 0;
+}

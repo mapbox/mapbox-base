f639b4aec43f15e08b30606a0539adcf099cc7ca
Bruno de Oliveira Abinader <bruno@mapbox.com>
2019-09-03 10:21:08 +0300

    [io] Convert io to static library

--- test/io_delete.cpp
+++ test/io_delete.cpp
@@ -1,4 +1,4 @@
-#include "../mapbox/io/include/mapbox/io.hpp"
+#include <mapbox/io.hpp>
 
 #include "io_delete.hpp"
 
@@ -8,13 +8,13 @@
 void deleteTests(const fspath& path, const fspath& copyPath, const fspath& invalidPath) {
     nonstd::expected<void, std::string> voidExpected;
 
-    voidExpected = mapbox::base::deleteFile(path);
+    voidExpected = mapbox::base::io::deleteFile(path);
     assert(voidExpected);
 
-    voidExpected = mapbox::base::deleteFile(copyPath);
+    voidExpected = mapbox::base::io::deleteFile(copyPath);
     assert(voidExpected);
 
-    voidExpected = mapbox::base::deleteFile(invalidPath);
+    voidExpected = mapbox::base::io::deleteFile(invalidPath);
     assert(!voidExpected);
     assert(voidExpected.error() == std::string("Failed to delete file 'invalid'"));
 }

507f32ed7e8bd4c5a477fbf7297f912e9d7f5bd2
Denis Koronchik <denis.koronchik@gmail.com>
2020-03-12 15:37:20 +0300

    Review fixes

--- scripts/ci/install-tests.sh
+++ scripts/ci/install-tests.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 mkdir build && pushd build
 export CC=${CCOMPILER} CXX=${CXXCOMPILER}
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_TESTS=ON
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_TESTING=ON
 echo "travis_fold:start:MAKE"
 make --jobs=${JOBS}
 echo "travis_fold:end:MAKE"
